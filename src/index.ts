dce14ef8a8cebef6a68c355af9e7f3a1bf25ba28
rikiTada <riki.tada777@gmail.com>
2025-07-16 21:36:05 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+import books from './books'
+
+const app = new Hono()
+
+app
+  .get('/', (c) => {
+    return c.json('Hello Hono!')
+  })
+  .route('/books', books)
+
+export default app
+// export type Apptype = typeof route
