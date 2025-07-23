884f672e808561df97955bea84fd9ae03ac7ec36
rikiTada <riki.tada777@gmail.com>
2025-07-23 23:01:41 +0900

    🍨 homeに本の一覧を表示

--- src/client/pages/home/HomePage.tsx
+++ src/client/pages/home/HomePage.tsx
@@ -1,34 +1,56 @@
 import { Layout } from '@/client/components/layout/Layout';
 import { options } from '@/db';
+import { SeriesWithBooks } from '@/db/types';
 import { drizzle } from 'drizzle-orm/d1';
-import { useEffect } from 'hono/jsx';
+import { css } from 'hono/css';
 
 export default async function HomePage(props: { messages: string[]; DB: D1Database }) {
   const db = drizzle(props.DB, options);
 
   const series = await db.query.series.findMany({ with: { books: true } });
 
-  const routers = [
-    { path: '/api/books', label: 'Books' },
-    { path: '/api/users', label: 'Users' },
-    { path: '/api/backup', label: 'Backup' },
-  ];
+  // const routers = [
+  //   { path: '/api/books', label: 'Books' },
+  //   { path: '/api/users', label: 'Users' },
+  //   { path: '/api/backup', label: 'Backup' },
+  // ];
 
   return (
     <Layout>
-      <h1>Hello Hono!</h1>
+      <h1>Book Vault!</h1>
 
-      <ul>
-        {routers.map((route) => {
-          return (
-            <li key={route.path}>
-              <a href={route.path}>{route.label}</a>
-            </li>
-          );
-        })}
-      </ul>
-
-      <pre className="p-4 rounded-md bg-zinc-300">{JSON.stringify(series, null, 2)}</pre>
+      <div className="">
+        <h2>Series List</h2>
+        {series.map((s) => (
+          <Series key={s.id} {...s} />
+        ))}
+      </div>
     </Layout>
   );
 }
+
+const headerClass = css`
+  background-color: #466fc9;
+  color: white;
+  padding: 1rem;
+`;
+
+const Series = (series: SeriesWithBooks) => {
+  return (
+    <div>
+      <h2 class={headerClass}>{series.title}</h2>
+
+      {series.books.length > 0 ? (
+        <ul>
+          {series.books.map((book) => (
+            <li key={book.id}>
+              <a href={book.targetUrl}>{book.title}</a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>- Nodata -</p>
+      )}
+    </div>
+  );
+};
